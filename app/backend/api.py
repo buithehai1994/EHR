4c69aebfdd83ac2dede6e762715117c6626e7c24
buithehai1994 <72820310+buithehai1994@users.noreply.github.com>
2025-01-12 12:13:42 +1100

    Update api.py

--- app/backend/api.py
+++ app/backend/api.py
@@ -1,31 +1,28 @@
 from fastapi import FastAPI, HTTPException
+import pandas as pd
+import io
 import requests
-import pickle
-
-# Dropbox link for direct access
-dropbox_link = "https://www.dropbox.com/scl/fi/kyihmcph1cfk3ynqsr2hn/filtered_df.pkl?rlkey=uzfs7lsir37hqywuxtlq1ko1r&st=hd66boln&dl=1"
 
 app = FastAPI()
 
-def load_pickle_from_dropbox(dropbox_link):
+# Dropbox link
+url = 'https://www.dropbox.com/scl/fi/kyihmcph1cfk3ynqsr2hn/filtered_df.pkl?rlkey=uzfs7lsir37hqywuxtlq1ko1r&st=hd66boln&dl=1'
+
+# Load the DataFrame once on app startup
+@app.on_event("startup")
+async def load_dataframe():
     try:
-        # Send a GET request to the Dropbox link
-        response = requests.get(dropbox_link, stream=True, timeout=10)
-        response.raise_for_status()  # Raise an error for bad responses
+        response = requests.get(url)
+        response.raise_for_status()  # Ensure the request was successful
+        global filtered_df
+        filtered_df = pd.read_pickle(io.BytesIO(response.content))
+    except requests.RequestException as e:
+        raise HTTPException(status_code=500, detail="Failed to load data") from e
 
-        # Load the content of the pickle file directly into memory
-        data = pickle.load(response.raw)
-
-        return data
-    except requests.exceptions.RequestException as e:
-        raise HTTPException(status_code=500, detail=f"An error occurred while accessing the file: {e}")
-    except pickle.PickleError as e:
-        raise HTTPException(status_code=500, detail=f"An error occurred while loading the pickle file: {e}")
-
-@app.get("/load_pickle")
-async def load_pickle_data():
-    data = load_pickle_from_dropbox(dropbox_link)
-    return {"message": "Pickle file loaded successfully", "data": data}
-
-# To run this application, use the command:
-# uvicorn app:app --reload
+# Endpoint to get the whole DataFrame
+@app.get("/get_whole_dataframe")
+async def get_whole_dataframe():
+    if 'filtered_df' in globals():
+        return filtered_df.to_dict(orient="records")
+    else:
+        raise HTTPException(status_code=404, detail="Data not loaded")
