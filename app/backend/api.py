5b1c0e0084417e831172082fd23fbf67966b537f
buithehai1994 <72820310+buithehai1994@users.noreply.github.com>
2025-01-12 15:09:22 +1100

    Update api.py

--- app/backend/api.py
+++ app/backend/api.py
@@ -1,28 +1,21 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 import pandas as pd
 import io
 import requests
 
+# Initialize the FastAPI app
 app = FastAPI()
 
-# Dropbox link
-url = 'https://www.dropbox.com/scl/fi/kyihmcph1cfk3ynqsr2hn/filtered_df.pkl?rlkey=uzfs7lsir37hqywuxtlq1ko1r&st=hd66boln&dl=1'
-
-# Load the DataFrame once on app startup
+# Load the DataFrame from Dropbox URL
 @app.on_event("startup")
 async def load_dataframe():
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # Ensure the request was successful
-        global filtered_df
-        filtered_df = pd.read_pickle(io.BytesIO(response.content))
-    except requests.RequestException as e:
-        raise HTTPException(status_code=500, detail="Failed to load data") from e
+    url = 'https://www.dropbox.com/scl/fi/kyihmcph1cfk3ynqsr2hn/filtered_df.pkl?rlkey=uzfs7lsir37hqywuxtlq1ko1r&st=hd66boln&dl=1'
+    response = requests.get(url)
+    response.raise_for_status()  # Ensure the request was successful
+    global filtered_df
+    filtered_df = pd.read_pickle(io.BytesIO(response.content))
 
-# Endpoint to get the whole DataFrame
-@app.get("/get_whole_dataframe")
-async def get_whole_dataframe():
-    if 'filtered_df' in globals():
-        return filtered_df.to_dict(orient="records")
-    else:
-        raise HTTPException(status_code=404, detail="Data not loaded")
+# Define an endpoint to display the DataFrame
+@app.get("/get_dataframe")
+async def get_dataframe():
+    return filtered_df.to_dict(orient="records")  # Return DataFrame as JSON records
