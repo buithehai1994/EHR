94c54b719c5914820f6f77c1e02bc4d92a6fa773
buithehai1994 <72820310+buithehai1994@users.noreply.github.com>
2025-01-13 13:47:18 +1100

    Update api.py

--- app/backend/api.py
+++ app/backend/api.py
@@ -4,26 +4,26 @@ import pandas as pd
 # Initialize the FastAPI app
 app = FastAPI()
 
-# Load the data using pandas
-file_path = 'data/filtered_df_part_1.parquet'
+# Define the file path
+file_path = 'data/chunk_1.json'
 
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to the FastAPI App for Loading Data"}
+    return {"message": "Welcome to the FastAPI App for Loading JSON Data"}
 
 # Endpoint to load the dataframe and display the first few rows
 @app.get("/load_data/")
 def load_and_display_data():
     try:
-        # Load the parquet file
-        data = pd.read_parquet(file_path, engine='pyarrow')
+        # Load the JSON file
+        data = pd.read_json(file_path)
         
         # Ensure the DataFrame is not empty
         if data.empty:
-            return {"error": "The parquet file is empty."}
+            return {"error": "The JSON file is empty."}
         
         # Convert DataFrame to a serializable format (JSON)
-        result = data.to_json(orient='records')
+        result = data.to_json(orient='records', indent=4)
         
         # Return the JSON response
         return {"data": result}
@@ -31,6 +31,6 @@ def load_and_display_data():
     except FileNotFoundError:
         return {"error": f"The file {file_path} does not exist."}
     except pd.errors.EmptyDataError:
-        return {"error": "The parquet file is empty."}
+        return {"error": "The JSON file is empty."}
     except Exception as e:
         return {"error": f"An error occurred: {str(e)}"}
